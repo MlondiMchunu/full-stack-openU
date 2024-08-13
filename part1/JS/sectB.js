ac2098109f0a3833db8b1009128661879cb7e6a4
Stopa <mlondiemchunu1@gmail.com>
2024-08-14 01:08:17 +0200

    commit objects

--- part1/JS/sectB.js
+++ part1/JS/sectB.js
@@ -28,17 +28,39 @@ t.forEach(value => {
     console.log(value)
 })
 
-const z = [4,8,2]
+const z = [4, 8, 2]
 
-const z1 = z.map((value)=>'<li>'+value * 2+'</li>')
+const z1 = z.map((value) => '<li>' + value * 2 + '</li>')
 
 console.log(z1)
 
 //destructuring assignment
-const t1 = [1,2,3,4,5,6,7,8]
+const t1 = [1, 2, 3, 4, 5, 6, 7, 8]
 
-const[first,second,third,...rest] = t1
+const [first, second, third, ...rest] = t1
 
 console.log(second)
 console.log(rest)
 
+/*Objects */
+
+const obj1 = {
+    name: 'Mlo',
+    age: 29,
+    education: 'BE',
+}
+
+const obj2 = {
+    name: 'Full Stack',
+    level: 'intermediate studies',
+    size: 5,
+}
+
+const obj3 = {
+    names:{
+        first:'Hector',
+        last:'Bellerin',
+    },
+    grades: [2,3,5,3],
+    dept:'SPPU',
+}
