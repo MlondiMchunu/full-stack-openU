054133431938945cf009635bed163fbc2515e477
Stopa <mlondiemchunu1@gmail.com>
2024-08-14 21:07:13 +0200

    commit react app

---
+++ part1/CompSt_EventHand/myapp/src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const model = "Lambo"
+  const year = 1970
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <div>
+        <h1> Car Stock</h1>
+        <Car model="Porsche" year={2022} />
+        <Car model={model} year={year} />
+      </div>
+    </div>
+  );
+}
+
+const Car = (props) => {
+  const manufactureYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+  return (
+    <>
+      <div>
+        <p>Model: {props.model} '<br />' Year: {props.year}</p>
+      </div>
+    </>
+  )
+}
+
+export default App;
