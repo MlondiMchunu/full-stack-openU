df14dd8715680010d982fdf41ddc904770db1e38
Stopa <mlondiemchunu1@gmail.com>
2024-08-15 12:10:01 +0200

    main.jsx edited

--- part1/CompSt_EventHand/myapp/src/main.jsx
+++ part1/CompSt_EventHand/myapp/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot, ReactDOM } from 'react-dom/client'
+import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+)
+
+let counter = 1
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App counter={counter}/>
 )
