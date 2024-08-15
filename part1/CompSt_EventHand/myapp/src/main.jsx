8ec8e09c0d10222ccc65aed43df1f9716a3f9db8
Stopa <mlondiemchunu1@gmail.com>
2024-08-15 13:35:07 +0200

    add StrctMode

--- part1/CompSt_EventHand/myapp/src/main.jsx
+++ part1/CompSt_EventHand/myapp/src/main.jsx
@@ -3,14 +3,17 @@ import { createRoot, ReactDOM } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+/*createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+*/
 
 let counter = 1
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
   <App counter={counter}/>
+  </StrictMode>
 )
