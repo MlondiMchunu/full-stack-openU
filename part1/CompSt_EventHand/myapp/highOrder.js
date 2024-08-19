04a25af3bdd7371f48a32dcbb84e25057cd1e294
Stopa <mlondiemchunu1@gmail.com>
2024-08-20 00:43:58 +0200

    commit remove species to map() function output

--- part1/CompSt_EventHand/myapp/highOrder.js
+++ part1/CompSt_EventHand/myapp/highOrder.js
@@ -47,7 +47,7 @@ for(var i = 0;i<animals.length;i++){
 */
 
 const names = animals.map(function(animal){
-    return animal.name + ' is a ' + animal.species
+    return animal.name 
 })
 
 console.log(names)
