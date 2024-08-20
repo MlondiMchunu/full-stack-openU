ab81d96ec237c39856930ce1dcbaa92f00c85eb5
Stopa <mlondiemchunu1@gmail.com>
2024-08-20 14:50:41 +0200

    create notes object

--- part2/app2/src/main.jsx
+++ part2/app2/src/main.jsx
@@ -2,3 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+
+const notes = [
+    {
+        id:1,
+        content:'HTML is easy',
+        important: true
+    },
+    {
+        id:2,
+        content:'Browser can execute only Javascript',
+        important:false
+    }
+]
