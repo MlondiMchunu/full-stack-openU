2b10f7769cc397c893f61886fe1742f956ebfe5d
Stopa <mlondiemchunu1@gmail.com>
2024-08-20 14:40:42 +0200

    latest changes

--- part2/app2/src/App.js
+++ part2/app2/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-const App=()=> {
+const App = (props) => {
+  const {notes} = props
   return (
     <>
-    <div className="App">
-    </div>
+      <div className="App">
+      </div>
+      <div>
+        <h1>Notes</h1>
+        <ul>
+          <li>{notes[0].content}</li>
+        </ul>
+      </div>
     </>
   );
 }
