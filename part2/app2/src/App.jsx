6943d20979d6091f11ea0438b68767ba717ae757
Stopa <mlondiemchunu1@gmail.com>
2024-09-03 15:02:08 +0200

    changes

--- part2/app2/src/App.jsx
+++ part2/app2/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
-import { response } from 'express'
+
 
 //import './App.css'
 
@@ -24,7 +24,6 @@ const App = () => {
   
 
 //effect hook code written differently
-
 
 
 
