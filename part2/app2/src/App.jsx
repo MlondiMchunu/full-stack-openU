6ec3521fb80055b4a7f8a621824f7f64dd451da2
Stopa <mlondiemchunu1@gmail.com>
2024-09-03 11:35:25 +0200

    refactoring render

--- part2/app2/src/App.jsx
+++ part2/app2/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
-import { response } from 'express'
+
 //import './App.css'
 
 const App = (props) => {
