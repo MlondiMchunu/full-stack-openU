317d6c8fe5119c774483e23f459365fe6b953850
Stopa <mlondiemchunu1@gmail.com>
2024-09-03 10:27:28 +0200

    event-handler for promise

--- part2/app2/src/main.jsx
+++ part2/app2/src/main.jsx
@@ -6,7 +6,12 @@ import axios from 'axios'
 
 
 const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
+promise.then(response=>{
+  console.log(response)
+})
+
+//const promise2 = axios.get('http://localhost:3001/foobar')
+//console.log(promise2)
 
 const notes = [
   {
