c6c801a3869d2f7d5e8de22bf3c94fed20840c48
Stopa <mlondiemchunu1@gmail.com>
2024-09-03 10:43:36 +0200

    chained promiose with .then

--- part2/app2/src/main.jsx
+++ part2/app2/src/main.jsx
@@ -5,13 +5,18 @@ import App from './App'
 import axios from 'axios'
 
 
-const promise = axios.get('http://localhost:3001/notes')
+/*const promise = axios.get('http://localhost:3001/notes')
 promise.then(response=>{
   console.log(response)
+})*/
+
+axios.get('http://localhost:3001/notes').then(response=>{
+  const notes = response.data
+  console.log(notes)
 })
 
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
+//onst promise2 = axios.get('http://localhost:3001/foobar')
+//console.log(promise2)
 
 const notes = [
   {
