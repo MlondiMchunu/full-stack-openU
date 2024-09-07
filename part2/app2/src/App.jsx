e2617c6ea40edd31ab09bd9c265bb0726a160fcf
Stopa <mlondiemchunu1@gmail.com>
2024-09-07 11:20:48 +0200

    latest commits

--- part2/app2/src/App.jsx
+++ part2/app2/src/App.jsx
@@ -10,8 +10,15 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-  const toggleImportanceOf = (id)=>{
-    console.log('importance of '+ id +' needs to be toggled')
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(n => n.id !== id ? n : response.data))
+    })
+    //console.log(`importance of ${id} needs to be toggled`)
   }
 
   /*useEffect(()=>{
@@ -93,9 +100,9 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} 
-                note={note}
-                toggleImportance={()=> toggleImportanceOf(note.id)} />
+          <Note key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)} />
         )}
       </ul>
       <form onSubmit={addNote}>
