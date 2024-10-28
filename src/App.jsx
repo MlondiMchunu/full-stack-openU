324ab7ecd2b81a1e8d22c9614ef5673355bc2e45
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-10-28 11:39:35 +0200

    forms

---
+++ src/App.jsx
@@ -0,0 +1,235 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
+import loginService from './services/login'
+
+import noteService from './services/notes'
+//import './App.css'
+
+const Footer =()=>{
+  const footerStyle ={
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return(
+    <div style={footerStyle}>
+      <br/>
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage,setErrorMessage] = useState(null)
+  const [username , setUsername] = useState('')
+  const [password , setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  
+
+   /*useEffect(()=>{
+    console.log('effect')
+    axios
+    .get('http://localhost:3001/notes')
+    .then(response=>{
+      console.log('promise fulfilled!')
+      setNotes(response.data)
+    })
+  },[])
+  */
+
+  //effect hook code written differently
+  /*const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  } */
+
+   //first parameter; the effect itself 
+  //2nd parameter; how often the effect is run
+  /*useEffect(hook, [])*/
+
+  /**Below code is optimization of the above */
+  useEffect(()=>{
+    console.log('effect')
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+        console.log("RESPOSE = ", initialNotes)
+      })
+      
+  },[])
+
+  console.log('render', notes.length, 'notes')
+
+
+  const toggleImportanceOf = (id) => {
+    //const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find((n) => { n.id === id })
+    const changedNote = { ...note, important: !note.important }
+
+    /*
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map((n) => { n.id !== id ? n : response.data }))
+    })
+      */
+    //console.log(`importance of ${id} needs to be toggled`)
+
+    /**Below code is optimization of above after extracting 
+     * communication module with backend */
+     noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `The note ${note.content} was already deleted from server!`
+        )
+        setNotes(notes.filter((n)=>{
+          n.id !== id
+        }))
+      })
+  }
+
+  /*const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      
+    }
+    //console.log('button clicked', event.target)
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }*/
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+    if (noteObject.content == '') {
+      console.log("This is an empty note!")
+    } else {
+  /*  axios
+        .post('http://localhost:3001/notes', noteObject)
+        .then(response => {
+
+          setNotes(notes.concat(response.data))
+          setNewNote('')
+          console.log(response)
+        })*/
+       noteService
+        .create(noteObject)
+        .then(returnedNote =>{
+          setNotes(notes.concat(returnedNote))
+          setNewNote('')
+        })
+    }
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  const handleLogin = async(event)=>{
+    event.preventDefault()
+    console.log('logging in with', username, password)
+
+    try{
+      const user = await loginService.login({
+        username, password
+      })
+    }catch(exception){
+      setErrorMessage('Wrong credentials')
+      setTimeout(()=>{
+        setErrorMessage(null)
+      },5000)
+    }
+  }
+
+  const loginForm = ()=>{
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({target})=> setUsername(target.value)}/>
+      </div>
+      <div>
+        password
+          <input
+           type="password"
+           value={password}
+           name="Password"
+           onChange={({target})=>setPassword(target.value)}/>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  }
+
+  const noteForm = ()=>(
+    <form onSubmit={addNote}>
+      <input
+       value = {newNote}
+       onChange={handleNoteChange}/>
+    </form>
+  )
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)} />
+        )}
+      </ul>
+      <form onSubmit={handleLogin}>
+        <div>
+          username : 
+           <input type="text" value={username} 
+                  name="Username" onChange={({target})=>setUsername(target.value)} />
+        </div>
+        <br></br>
+        <div>
+          password : 
+           <input 
+           type = "password"
+           value={password} name="Password" onChange={({target})=> setPassword(target.value)}/>
+        </div>
+        <button type="submit">login</button>
+      </form>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit" >save</button>
+      </form>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/services/notes.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/notes'
+
+const getAll = () => {
+    //return axios.get(baseUrl)
+    const request = axios.get(baseUrl)
+
+    const nonExisting = {
+        id: 10000,
+        content: 'This note is not saved to server!',
+        important: true,
+    }
+    //return request.then(response => response.data.concat(nonExisting))
+    return request.then(respons => {
+        return respons.data
+    })
+    //return request.then(response => response.data.concat(nonExisting))
+    //return request.then(response => response.data)
+}
+
+const create = newObject => {
+    //return axios.post(baseUrl, newObject)
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response)
+}
+
+const update = (id, newObject) => {
+    //return axios.put(`${baseUrl}/${id}`,newObject)
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response)
+}
+
+export default { getAll, create, update }
