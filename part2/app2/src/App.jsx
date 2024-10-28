3eae016755a33cb4b0f07de07b148de3909a8526
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-10-28 13:34:27 +0200

    changes

--- part2/app2/src/App.jsx
+++ part2/app2/src/App.jsx
@@ -1,232 +1,148 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
-import loginService from './services/login'
-import noteService from './services/notes'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
-//import './App.css'
+import noteService from './services/notes'
+import loginService from './services/login'
 
 const App = () => {
-
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage,setErrorMessage] = useState(null)
-  const [username , setUsername] = useState('')
-  const [password , setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  
 
-   /*useEffect(()=>{
-    console.log('effect')
-    axios
-    .get('http://localhost:3001/notes')
-    .then(response=>{
-      console.log('promise fulfilled!')
-      setNotes(response.data)
-    })
-  },[])
-  */
-
-  //effect hook code written differently
-  /*const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        setNotes(response.data)
-      })
-  } */
-
-   //first parameter; the effect itself 
-  //2nd parameter; how often the effect is run
-  /*useEffect(hook, [])*/
-
-  /**Below code is optimization of the above */
-  useEffect(()=>{
-    console.log('effect')
+  useEffect(() => {
     noteService
       .getAll()
       .then(initialNotes => {
         setNotes(initialNotes)
-        console.log("RESPOSE = ", initialNotes)
       })
-      
-  },[])
-
-  console.log('render', notes.length, 'notes')
-
-
-  const toggleImportanceOf = (id) => {
-    //const url = `http://localhost:3001/notes/${id}`
-    const note = notes.find((n) => { n.id === id })
-    const changedNote = { ...note, important: !note.important }
-
-    /*
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map((n) => { n.id !== id ? n : response.data }))
-    })
-      */
-    //console.log(`importance of ${id} needs to be toggled`)
-
-    /**Below code is optimization of above after extracting 
-     * communication module with backend */
-     noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-      })
-      .catch(error => {
-        alert(
-          `The note ${note.content} was already deleted from server!`
-        )
-        setNotes(notes.filter((n)=>{
-          n.id !== id
-        }))
-      })
-  }
-
-  /*const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      
-    }
-    //console.log('button clicked', event.target)
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }*/
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
+      important: Math.random() > 0.5,
     }
-    if (noteObject.content == '') {
-      console.log("This is an empty note!")
-    } else {
-  /*  axios
-        .post('http://localhost:3001/notes', noteObject)
-        .then(response => {
+  
+    noteService
+      .create(noteObject)
+        .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
 
-          setNotes(notes.concat(response.data))
-          setNewNote('')
-          console.log(response)
-        })*/
-       noteService
-        .create(noteObject)
-        .then(returnedNote =>{
-          setNotes(notes.concat(returnedNote))
-          setNewNote('')
-        })
-    }
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(id, changedNote)
+        .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      })
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
   const notesToShow = showAll
     ? notes
-    : notes.filter(note => note.important === true)
+    : notes.filter(note => note.important)
 
-  const handleLogin = async(event)=>{
-    event.preventDefault()
-    console.log('logging in with', username, password)
-
-    try{
-      const user = await loginService.login({
-        username, password
-      })
-    }catch(exception){
-      setErrorMessage('Wrong credentials')
-      setTimeout(()=>{
-        setErrorMessage(null)
-      },5000)
-    }
-  }
-
-  const loginForm = ()=>{
+  const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
         username
           <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({target})=> setUsername(target.value)}/>
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
       </div>
       <div>
         password
           <input
-           type="password"
-           value={password}
-           name="Password"
-           onChange={({target})=>setPassword(target.value)}/>
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
       </div>
       <button type="submit">login</button>
-    </form>
-  }
-
-  const noteForm = ()=>(
+    </form>      
+  )
+  
+  const noteForm = () => (
     <form onSubmit={addNote}>
       <input
-       value = {newNote}
-       onChange={handleNoteChange}/>
-    </form>
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>  
   )
 
   return (
     <div>
       <h1>Notes</h1>
-      <Notification message={errorMessage}/>
+      <Notification message={errorMessage} />
 
-      {/*React tricks to render forms conditionally
-      //{user === null && loginForm()}
-      //{user !== null && noteForm()} */}
+      {!user && loginForm()}
+      {user && <div>
+       <p>{user.name} logged in</p>
+         {noteForm()}
+      </div>
+    } 
 
-      {user === null ? 
-      loginForm() : 
-        <div> 
-           <p>{user.name} logged-in </p>
-        {noteForm()}
-        </div>}
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
+          show {showAll ? 'important' : 'all' }
         </button>
-      </div>
+      </div>      
       <ul>
-        {notesToShow.map(note =>
-          <Note key={note.id}
+        {notesToShow.map(note => 
+          <Note
+            key={note.id}
             note={note}
-            toggleImportance={() => toggleImportanceOf(note.id)} />
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
-      <form onSubmit={handleLogin}>
-        <div>
-          username : 
-           <input type="text" value={username} 
-                  name="Username" onChange={({target})=>setUsername(target.value)} />
-        </div>
-        <br></br>
-        <div>
-          password : 
-           <input 
-           type = "password"
-           value={password} name="Password" onChange={({target})=> setPassword(target.value)}/>
-        </div>
-        <button type="submit">login</button>
-      </form>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit" >save</button>
-      </form>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
