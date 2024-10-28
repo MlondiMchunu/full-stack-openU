e84f943456eade2c3f66fada9142202ab920eb37
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-10-28 15:29:01 +0200

    refactor lost code

--- part2/app2/src/services/notes.js
+++ part2/app2/src/services/notes.js
@@ -1,33 +1,21 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
-    //return axios.get(baseUrl)
-    const request = axios.get(baseUrl)
-
-    const nonExisting = {
-        id: 10000,
-        content: 'This note is not saved to server!',
-        important: true,
-    }
-    //return request.then(response => response.data.concat(nonExisting))
-    return request.then(respons => {
-        return respons.data
-    })
-    //return request.then(response => response.data.concat(nonExisting))
-    //return request.then(response => response.data)
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
 }
 
 const create = newObject => {
-    //return axios.post(baseUrl, newObject)
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => response)
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
-    //return axios.put(`${baseUrl}/${id}`,newObject)
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response)
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
 }
 
-export default { getAll, create, update }
+export default { 
+  getAll, create, update 
+}
